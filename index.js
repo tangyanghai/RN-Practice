60c6c092cd363adb8153dac59807f962379264f2
tangyanghai <tangyanghai@wshifu.com>
2022-01-27 18:00:13 +0800

    抽出测试工程的默认首页

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import DefaultApp from "./src/default/DefaultApp";
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => DefaultApp);
